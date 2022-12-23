95f0a95ba3346a7c48cb9c2d12ed6cd0372078f3
fidahussainqazafi <fidahussainqazafi60@gmail.com>
2022-12-23 17:16:37 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/fluttescratch/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fluttescratch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen_two.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class ScreenTwo extends StatefulWidget {
+  const ScreenTwo({Key? key}) : super(key: key);
+
+  @override
+  State<ScreenTwo> createState() => _ScreenTwoState();
+}
+
+class _ScreenTwoState extends State<ScreenTwo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Navigation Bar'),
+        backgroundColor: Colors.cyan,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: TextButton(
+              onPressed: (){
+
+              },
+
+              child: Text('Screen 2'),
+            ),
+          )
+        ],
+
+      ),
+
+    );
+
+  }
+}
